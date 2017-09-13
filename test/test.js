5f0ece2c85b2e7098986febfd7a3a668c888f59d
Alex Sabella <asabs14@gmail.com>
2017-09-13 19:06:36 -0400

    Fix Test Bug
    - Fixed removeCollection in test to be dropCollection

--- test/test.js
+++ test/test.js
@@ -2,15 +2,15 @@
 const { Dex } = require('../build/Dex');
 
 let db = new Dex("ws://localhost:8080/");
-db.collection("stuff").find({field1: "value1"})
+/*db.collection("stuff").find({field1: "value1"})
     .and(Dex.and({field2: "value3"}, {field5: "value5"}), {field6: Dex.lt("value6")})
     .or(Dex.or({field3: ["value2", "valuelots"]}, {field4: Dex.gt_lte(1, 5)}), {field7: "value7"})
     .or({num: 0})
     .count();
+*/
+//db.collection("stuff").find({field1: "value1"}).fetch();
 
-db.collection("stuff").find({field1: "value1"}).fetch();
-
-db.collection("stuff").removeCollection();
+db.collection("stuff").dropCollection();
 db.removeCollection("stuff");
 
 db.collection("stuff").insert({this: "that"}, {that: 1});
